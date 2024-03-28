764534b89bd252b8b97d428ae04b0a07096d608f
Twaters623 <142362719+Twaters623@users.noreply.github.com>
2024-03-28 23:42:19 +0000

    idk

---
+++ gauss_sum/linearSum.js
@@ -0,0 +1,18 @@
+function maxInt(a){
+    let totalSum = 0;
+    for (i = 0; i <= a; i++ ){
+        totalSum += i;
+
+    }
+    return totalSum;
+
+
+}
+console.log(maxInt(100));
+
+        
+
+    
+
+
+
